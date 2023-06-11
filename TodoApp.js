56b55db9511bdb0a4e32e3e3700715d81738bb68
IamSmartDev <dsmartwinny@gmail.com>
2023-06-12 00:03:55 +0100

    final implementation of desired features for best practices

--- TodoApp.js
+++ TodoApp.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './TodoApp.css';
-
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([
@@ -8,25 +9,26 @@ const TodoApp = () => {
       id: 1,
       title: 'Coding Prep',
       description: 'By the end of today, I should have solved over 10 coding challenges',
-      completed: false
+      completed: false,
     },
     {
       id: 2,
       title: 'Work Out',
       description: 'In order to stay fit, I must complete at least 200 push-ups',
-      completed: true
+      completed: true,
     },
     {
       id: 3,
       title: 'Task Delivery',
-      description: 'I will complete ant submit any pending task for today',
-      completed: false
-    }
+      description: 'I will complete and submit any pending task for today',
+      completed: false,
+    },
   ]);
+
   const [newTodo, setNewTodo] = useState({
     title: '',
     description: '',
-    completed: false
+    completed: false,
   });
 
   useEffect(() => {
@@ -43,35 +45,45 @@ const TodoApp = () => {
   const handleInputChange = (e) => {
     setNewTodo({
       ...newTodo,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const handleAddTodo = () => {
     if (newTodo.title.trim() !== '') {
-      const todo = {
-        id: Date.now(),
-        title: newTodo.title,
-        description: newTodo.description,
-        completed: newTodo.completed
-      };
+      if (newTodo.description.trim() !== '') {
+        const todo = {
+          id: Date.now(),
+          title: newTodo.title,
+          description: newTodo.description,
+          completed: newTodo.completed,
+        };
 
-      setTodos([...todos, todo]);
-      setNewTodo({
-        title: '',
-        description: '',
-        completed: false
-      });
+        setTodos([...todos, todo]);
+        setNewTodo({
+          title: '',
+          description: '',
+          completed: false,
+        });
+
+        toast.success('Todo added successfully');
+      } else {
+        toast.error('Please enter a description');
+      }
+    } else {
+      toast.error('Please enter a title');
     }
   };
 
   const handleToggleStatus = (id) => {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
-        return {
+        const updatedTodo = {
           ...todo,
-          completed: !todo.completed
+          completed: !todo.completed,
         };
+        toast.info(`Todo ${updatedTodo.completed ? 'completed' : 'uncompleted'}`);
+        return updatedTodo;
       }
       return todo;
     });
@@ -82,26 +94,30 @@ const TodoApp = () => {
   const handleDeleteTodo = (id) => {
     const updatedTodos = todos.filter((todo) => todo.id !== id);
     setTodos(updatedTodos);
+
+    toast.success('Todo deleted successfully');
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Todo App</h1>
-      <input
-        type="text"
-        name="title"
-        value={newTodo.title}
-        onChange={handleInputChange}
-        placeholder="Title"
-      />
-      <input
-        type="text"
-        name="description"
-        value={newTodo.description}
-        onChange={handleInputChange}
-        placeholder="Description"
-      />
-      <button onClick={handleAddTodo}>Add Todo</button>
+      <div className="input-group">
+        <input
+          type="text"
+          name="title"
+          value={newTodo.title}
+          onChange={handleInputChange}
+          placeholder="Title"
+        />
+        <input
+          type="text"
+          name="description"
+          value={newTodo.description}
+          onChange={handleInputChange}
+          placeholder="Description"
+        />
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </div>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
@@ -110,13 +126,20 @@ const TodoApp = () => {
               checked={todo.completed}
               onChange={() => handleToggleStatus(todo.id)}
             />
-            {todo.title} - {todo.description}
-            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            <div className="todo-details">
+              <h3 className="title">{todo.title}</h3>
+              <p className="description">{todo.description}</p>
+            </div>
+            <button className="delete-button" onClick={() => handleDeleteTodo(todo.id)}>
+              Delete
+            </button>
           </li>
         ))}
       </ul>
+      <ToastContainer />
     </div>
   );
 };
 
 export default TodoApp;
+
